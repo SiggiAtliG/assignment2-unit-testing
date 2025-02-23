2b0d7e63ddcdd0681b0ef01febcdb11741859ff0
Sigurdur Atli Gudmundsson <sigurduratlig@gmail.com>
2025-02-23 19:58:59 +0000

    style: fix formatting issues with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,41 @@
 module.exports = {
-root: true,
-env: {
+  root: true,
+  env: {
     node: true,
-    es2020: true
-},
-extends: [
-    "eslint:recommended"
-],
-parserOptions: {
-    ecmaVersion: 2020
-},
-overrides: [
+    es2020: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  overrides: [
     // TypeScript files
     {
-    files: ["*.ts", "*.tsx"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: [
-        "plugin:@typescript-eslint/recommended"
-    ],
-    parserOptions: {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      parserOptions: {
         project: "./tsconfig.json",
         tsconfigRootDir: __dirname,
         ecmaVersion: 2020,
-        sourceType: "module"
-    }
+        sourceType: "module",
+      },
     },
     // JavaScript files
     {
-    files: ["*.js"],
-    env: {
+      files: ["*.js"],
+      env: {
         node: true,
-        commonjs: true
-    },
-    parserOptions: {
+        commonjs: true,
+      },
+      parserOptions: {
         ecmaVersion: 2020,
-        sourceType: "module"
+        sourceType: "module",
+      },
+      rules: {
+        "no-undef": "off", // Since these are Node.js files using CommonJS
+      },
     },
-    rules: {
-        "no-undef": "off"  // Since these are Node.js files using CommonJS
-    }
-    }
-]
+  ],
 };
--- eslint-rules/__tests__/camelCaseFunctions.test.js
+++ eslint-rules/__tests__/camelCaseFunctions.test.js
@@ -1,32 +1,32 @@
-import { describe, test } from 'vitest';
+import { describe, test } from "vitest";
 import { RuleTester } from "eslint";
 import rule from "../camelCaseFunctions";
-describe('camelCaseFunctions ESLint Rule', () => {
-test('validates and fixes function naming conventions', () => {
+describe("camelCaseFunctions ESLint Rule", () => {
+  test("validates and fixes function naming conventions", () => {
     const ruleTester = new RuleTester({
-    parserOptions: {
+      parserOptions: {
         ecmaVersion: 2021,
-        sourceType: "module"
-    }
+        sourceType: "module",
+      },
     });
 
     ruleTester.run("camelCaseFunctions", rule, {
-    valid: [
+      valid: [
         { code: "function myFunction() {}" },
         { code: "const myFunction = () => {};" },
-    ],
-    invalid: [
+      ],
+      invalid: [
         {
-        code: "function My_function() {}",
-        output: "function myFunction() {}",
-        errors: [{ message: "Function names should be in camelCase." }],
+          code: "function My_function() {}",
+          output: "function myFunction() {}",
+          errors: [{ message: "Function names should be in camelCase." }],
         },
         {
-        code: "const my_function = () => {};",
-        output: "const myFunction = () => {};",
-        errors: [{ message: "Function names should be in camelCase." }],
+          code: "const my_function = () => {};",
+          output: "const myFunction = () => {};",
+          errors: [{ message: "Function names should be in camelCase." }],
         },
-    ],
+      ],
     });
-});
+  });
 });
